50663c0fea0fdc7f54282b2dbb783415b8342b86
Erik Schuessler <90277878+Brainspeck@users.noreply.github.com>
2026-05-20 09:02:39 +0000

    implement fetch and asyn functions to the project

--- src/js/components/ToDos.jsx
+++ src/js/components/ToDos.jsx
@@ -1,19 +1,99 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./ToDos.css"
+
 
 const ToDo = () => {
     const [inputValue, setInputValue] = useState('');
     const [toDos, setTodos] = useState([]);
+    const userName =  'stefan';
+    
+    
 
-    const addTask = () => {
-        if (inputValue.trim() === "") {return}
-        setTodos([...toDos, inputValue.trim()])
-        setInputValue('');
+    useEffect(() => {
+        const init = async () => {
+            const userExists = await getTasks();
+            if(!userExists) {
+                await createUser(userName);
+                await getTasks();
+            }
+        };
+        init();
+    }, [])
+    
+
+
+    const getTasks = async () => {
+        try {
+            const response = await fetch(`https://playground.4geeks.com/todo/users/${userName}`);
+            if (!response.ok) {
+                throw new Error (`${response.status} ${response.statusText}`)
+            }
+            const data = await response.json();
+            setTodos(data.todos)
+            return true;
+        } 
+        
+        catch (error) {
+            console.error("Error al obtener datos:", error)
+            return false;
+        }
     }
 
-    const removeToDo = (indexToDelete) => {
-        const remainingTasks = toDos.filter((task,index) => index !== indexToDelete)
-        setTodos(remainingTasks);
+
+    const createUser = async () => {
+        try {
+            const response = await fetch (`https://playground.4geeks.com/todo/users/${userName}`, {
+                method: 'POST',
+            });
+            if (!response.ok) {
+                const error = await response.json();
+                throw new Error (error.message || 'Error al crear el usuario')
+            }
+            const data = await response.json();
+            return data;
+        }
+        catch (error){
+            console.error('Error:', error);
+            throw error;
+        }
+    };
+
+    const addTask = async () => {
+        const newTask = {
+            label: inputValue.trim(),
+            is_done: false
+        };
+       try {
+        const response = await fetch(`https://playground.4geeks.com/todo/todos/${userName}`, {
+            method: 'POST',
+             headers: {
+                "Content-Type": "application/json"
+             },
+            body: JSON.stringify(newTask)
+        });
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error (error.message || 'Error al crear la tarea');
+        };
+        const data = await response.json();
+        await getTasks();
+        setInputValue("");
+        return data;
+       }
+       catch (error) {
+        console.error('Error:', error);
+        throw error;
+       };
+    };
+
+    const removeToDo = async (toDoId) => {
+        const response = await fetch(`https://playground.4geeks.com/todo/todos/${toDoId}`, {
+            method: 'DELETE',
+        });
+        if (!response.ok) {
+             throw new Error("Error al eliminar la tarea");
+        }
+       await getTasks();
     }
 
     return(
@@ -32,11 +112,11 @@ const ToDo = () => {
             />
             <div className="task-container">
                 {toDos.length === 0 ? <div className="empty">No hay tareas, añadir tareas</div> : null}
-                { toDos.map((toDo,index) => {
+                { toDos.map((toDo) => {
                     return(
-                        <div key={index} className="task">
-                            <span>{toDo}</span>
-                            <button className="delete-button" onClick={() => {removeToDo(index)}}>X</button>
+                        <div key={toDo.id} className="task">
+                            <span>{toDo.label}</span>
+                            <button className="delete-button" onClick={() => {removeToDo(toDo.id)}}>X</button>
                         </div>
                     )
                 })}  
